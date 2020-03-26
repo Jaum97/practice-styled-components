2d2c3e4a42e1497186176c82b09fdea34865166c
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-03-25 23:50:27 -0300

    Create mediaqueries.tsx

---
+++ mediaqueries.tsx
@@ -0,0 +1,9 @@
+const ColorChanger = styled.section`
+  background: papayawhip;
+  color: palevioletred;
+
+  @media(min-width: 768px) {
+    background: mediumseagreen;
+    color: papayawhip;
+  }
+`;
